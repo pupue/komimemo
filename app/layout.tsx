776be95ff78f912b6a758cf32401b7467f90bdf4
komiyama029 <komiyama@ekanlab.co.jp>
2023-06-09 14:25:30 +0900

    update:Google Fonts導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/Footer';
 import Nav from '@/components/Nav';
 import './globals.css';
 import styles from './layout.module.css';
+import { Nunito, Noto_Sans_JP, M_PLUS_Rounded_1c } from 'next/font/google';
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
@@ -20,6 +21,23 @@ export const metadata = {
   },
 };
 
+const mplus = M_PLUS_Rounded_1c({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-mplus',
+});
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const nunito = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 type Props = {
   children: React.ReactNode;
 };
@@ -29,7 +47,7 @@ export default async function RootLayout({ children }: Props) {
     limit: LIMIT,
   });
   return (
-    <html lang="ja">
+    <html lang="ja" className={`${nunito.className} ${notoSansJP.className} ${mplus.variable}`}>
       <body>
         <Header />
         <Nav tags={tags.contents} />
