2eaac8d0640cdd9631f1e1973ce434cad06a2093
komiyama029 <komiyama@ekanlab.co.jp>
2023-06-09 14:37:37 +0900

    change:Header、Footerテキスト変更

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,15 +5,8 @@ import styles from './index.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <Image
-          src="/logo.svg"
-          alt="SIMPLE"
-          className={styles.logo}
-          width={348}
-          height={133}
-          priority
-        />
+      <Link href="/" className={styles.logo}>
+        Komimemo
       </Link>
     </header>
   );
