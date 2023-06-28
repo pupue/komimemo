d7cca9bd032c0b8b6d9e919386eecc3c54d89a7c
komiyama029 <komiyama@ekanlab.co.jp>
2023-06-28 18:09:57 +0900

    一時的にcontactページを閉じる

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,14 @@
+'use client';
 import Form from '@/components/Form';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default async function Page() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/');
+  }, [router]);
+
   return (
     <div className="text-3xl">
       <Form />
