3cfc60d5f83be56cda777083361468f140df397f
komiyama029 <komiyama@ekanlab.co.jp>
2023-07-05 21:52:19 +0900

    #23 PaginationコンポーネントをTailwind CSSに変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { getList } from '@/libs/microcms/client';
 import { LIMIT } from '@/constants';
-import Pagination from '@/components/Pagination';
+import { Pagination } from '@/components/Pagination';
 import { ArticleList } from '@/components/ArticleList';
 import { LottieAnimation } from '@/components/LottieAnimation';
 
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,6 @@
 import { getList } from '@/libs/microcms/client';
 import { ArticleList } from '@/components/ArticleList';
-import Pagination from '@/components/Pagination';
+import { Pagination } from '@/components/Pagination';
 
 type Props = {
   searchParams: {
