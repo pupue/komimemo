bbed4dff90c8c1dfa6f82c9f79d61ac810c8bb47
komiyama029 <komiyama@ekanlab.co.jp>
2023-07-05 22:33:34 +0900

    ビルドエラー解消

--- app/tags/[tagId]/layout.tsx
+++ app/tags/[tagId]/layout.tsx
@@ -1,6 +1,6 @@
 import styles from './layout.module.css';
 import { getTag } from '@/libs/microcms/functions';
-import { TagListItem } from '@/components/TagListItem';
+import { TagListItem } from '@/components/posts/TagListItem';
 
 type Props = {
   children: React.ReactNode;
