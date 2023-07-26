ec588565e9b24f7bc76535bec73e99fbb2ee0c79
komiyama029 <komiyama@ekanlab.co.jp>
2023-07-26 11:56:49 +0900

    #30 bugfix:ダークモード挙動修正

--- components/theme/ThemeChanger.tsx
+++ components/theme/ThemeChanger.tsx
@@ -1,24 +1,20 @@
 'use client';
+
 import { useTheme } from 'next-themes';
 import { LightModeIcon, DarkModeIcon } from '@/components/ui/icons/index';
 
 export const ThemeChanger = () => {
   const { theme, setTheme } = useTheme();
 
-  const handleClick = (theme: string) => {
-    setTheme(theme);
-    localStorage.setItem('theme', theme);
-  };
-
   return (
     <div>
       {theme === 'light' ? (
-        <button onClick={() => handleClick('dark')}>
-          <LightModeIcon />
+        <button className="sun" onClick={() => setTheme('dark')}>
+          <DarkModeIcon />
         </button>
       ) : (
-        <button onClick={() => handleClick('light')}>
-          <DarkModeIcon />
+        <button className="moon" onClick={() => setTheme('light')}>
+          <LightModeIcon />
         </button>
       )}
     </div>
