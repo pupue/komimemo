f5d3a35fc77bd6a855ddc7623355d6f924db6f22
pupue <reika02955@gmail.com>
2025-08-21 16:04:14 +0900

    フォント変更（エラーでるので暫定対応）

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,6 +1,6 @@
-import { Nunito, Noto_Sans_JP, M_PLUS_Rounded_1c } from 'next/font/google';
+import { Nunito, Noto_Sans_JP, M_PLUS_1 } from 'next/font/google';
 
-export const mplus = M_PLUS_Rounded_1c({
+export const mplus = M_PLUS_1({
   weight: ['400', '700'],
   subsets: ['latin'],
   display: 'swap',
