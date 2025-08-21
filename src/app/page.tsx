da420a4d51cd813654e4e25b79ac33280a995542
pupue <reika02955@gmail.com>
2025-08-21 15:57:55 +0900

    cheaperバナー追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { LIMIT } from '@/constants';
 import { Pagination } from '@/components/posts/Pagination';
 import { ArticleList } from '@/components/posts/ArticleList';
 import { LottieAnimation } from '@/components/LottieAnimation';
+import { DownIcon } from '@/components/ui/icons/down-icon';
 
 export const revalidate = 60;
 
@@ -14,6 +15,33 @@ export default async function Page() {
     <>
       <LottieAnimation />
       <p className="text-center mb-12">日々の学びをアウトプットしていくよ</p>
+      <div className="text-sm border border-[#333] dark:border-white">
+        <details>
+          <summary className="flex items-center justify-between p-4 cursor-pointer list-none">
+            <span>
+              どっちが安いか比較するアプリ{' '}
+              <a
+                href="https://cheaper.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+              >
+                cheaper
+              </a>{' '}
+              をリリースしました
+            </span>
+            <span className="down-icon">
+              <DownIcon />
+            </span>
+          </summary>
+          <div className="px-4">
+            <div className="border-t border-dashed py-4">
+              <p>どっちが安いか毎回電卓で計算するのが面倒という母（と自分）のために作りました。</p>
+              <p>毎回色が変わるブタちゃんからのメッセージに癒されてください。</p>
+            </div>
+          </div>
+        </details>
+      </div>
       <ArticleList articles={data.contents} />
       <Pagination totalCount={data.totalCount} />
     </>
---
+++ src/components/ui/icons/down-icon.tsx
@@ -0,0 +1,19 @@
+export const DownIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-chevron-down-icon lucide-chevron-down"
+    >
+      <title>Down Icon</title>
+      <path d="m6 9 6 6 6-6" />
+    </svg>
+  );
+};
